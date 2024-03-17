fd9fb4ac15e12306c4379cbfbc1e890b197ac5aa
SaraCastril1 <smcastril1@eafit.edu.co>
2024-03-17 16:44:49 -0500

    delete saas

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,5 @@ Route::get('/', 'App\Http\Controllers\HomeController@index')->name('home.index')
 
 // Admin routes --------------------------------
 Route::get('/admin', 'App\Http\Controllers\Admin\AdminHomeController@index')->name("admin.home.index");
-
-
 Auth::routes();
 
