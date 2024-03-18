4ea61d701b00fd6fc4fde7abcc33a50ba84a2640
SaraCastril1 <smcastril1@eafit.edu.co>
2024-03-18 17:18:20 -0500

    feat: admin + cart

---
+++ app/Http/Controllers/Admin/AdminProductController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class AdminProductController extends Controller
+{
+    public function index(): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Admin Page - Products - Online Store';
+        $viewData['products'] = Product::all();
+
+        return view('admin.product.index')->with('viewData', $viewData);
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        Product::validate($request); 
+
+        $newProduct = new Product();
+        $newProduct->setName($request->input('name'));
+        $newProduct->setDescription($request->input('description'));
+        $newProduct->setCategory($request->input('category'));
+        $newProduct->setPrice($request->input('price'));
+        $newProduct->setStock($request->input('stock'));
+        $newProduct->setImage('game.png');
+        $newProduct->save();
+
+        return back();
+    }
+
+    public function delete($id): RedirectResponse
+    {
+        Product::destroy($id);
+
+        return back();
+    }
+
+    public function edit($id): View
+    {
+        $viewData = [];
+        $viewData['title'] = 'Admin Page - Edit Product - Online Store';
+        $viewData['product'] = Product::findOrFail($id);
+
+        return view('admin.product.edit')->with('viewData', $viewData);
+    }
+
+    public function update(Request $request, $id): RedirectResponse
+    {
+        Product::validate($request); 
+
+        $product = Product::findOrFail($id);
+        $product->setName($request->input('name'));
+        $product->setDescription($request->input('description'));
+        $product->setPrice($request->input('price'));
+
+        $product->save();
+
+        return redirect()->route('admin.product.index');
+    }
+}
